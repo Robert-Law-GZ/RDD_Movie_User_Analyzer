1b63c1c4674daeaf309bb24b7ab8a485e5867fbe
admin <admin>
2018-07-22 16:05:21 +0800

    修改查询条件

--- src/main/scala/UsersAnalyzer.scala
+++ src/main/scala/UsersAnalyzer.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.types._
 
 object UsersAnalyzer {
-// 数据文件下载地址：http://files.grouplens.org/datasets/movielens/ml-20m.zip
+  // 数据文件下载地址：http://files.grouplens.org/datasets/movielens/ml-20m.zip
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[*]").setAppName("Movie Users Analyzer")
@@ -12,20 +12,18 @@ object UsersAnalyzer {
     sc.setLogLevel("warn")
     val dataPath = "/usr/local/Cellar/spark/data/ml-20m/"
     val usersRDD = sc.textFile(dataPath + "tags.csv")
+
     //    userId,movieId,tag,timestamp
     val schemaForUsers = StructType("UserId::MovieId::tag::timestamp".split("::").map(column => StructField(column, StringType, true)))
     val usersRDDRows = usersRDD.map(_.split(",")).map(line => Row(line(0).trim, line(1).trim, line(2).trim, line(3).trim))
     val userDataFrame = spark.createDataFrame(usersRDDRows, schemaForUsers);
 
-    bySQL(spark,userDataFrame)
+    byTest(spark, userDataFrame)
+    bySQL(spark, userDataFrame)
+    byDataFrame(spark, userDataFrame)
 
-    //    val schemaForRatings=StructType("UserID::MovieID".split("::").map(column=>StructField(column,StringType,true))).add("Rating",DoubleType,true).add("Timestamp",StringType,true)
-    val groups = userDataFrame.filter(s"UserId!='userId'").groupBy("tag").count()
-    groups.orderBy(-groups("count")).show(10)
-
-    //    userDataFrame.show(10)
     spark.stop()
-}
+  }
 
   def bySQL(spark: SparkSession, df: DataFrame): Unit = {
     df.createTempView("users");
@@ -33,8 +31,13 @@ object UsersAnalyzer {
     spark.sql(sql).show(10);
   }
 
-  def byDataFrame(): Unit = {
-
+  def byDataFrame(spark: SparkSession, df: DataFrame): Unit = {
+    val groups = df.filter(s"UserId!='userId'").groupBy("tag").count()
+    groups.orderBy(-groups("count")).show(10)
   }
 
+  def byTest(spark: SparkSession, df: DataFrame): Unit = {
+    val movieTotal=df.select("MovieId").where("MovieId!='movieId'").groupBy("MovieId").count()
+    movieTotal.orderBy(-movieTotal("count")).show(10)
+  }
 }
