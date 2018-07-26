4f19d8704527f0dc38325fe452cd1ddc1ad5aa88
luoqiang <65605684>
2018-07-26 18:18:31 +0800

    添Hbase的insert操作

--- src/main/scala/InsertDataToHbase.scala
+++ src/main/scala/InsertDataToHbase.scala
@@ -1,8 +1,11 @@
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.hadoop.hbase.client.{ConnectionFactory, HTable, Put}
-import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.hbase.client.{ConnectionFactory, HTable, Put, Result}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object InsertDataToHbase {
   def main(args: Array[String]): Unit = {
@@ -15,18 +18,27 @@ object InsertDataToHbase {
     //设置查询的表名
     conf.set(TableInputFormat.INPUT_TABLE, table_name)
 
-    val conn = ConnectionFactory.createConnection(conf)
-    val tn=TableName.valueOf(table_name);
+//    val conn = ConnectionFactory.createConnection(conf)
+//    val tn=TableName.valueOf(table_name);
 
-    val table = conn.getTable(tn);
+//    val table = conn.getTable(tn);
+//
+//    val p = new Put(new String("4").getBytes)
+//    p.addColumn("info".getBytes(), "name".getBytes(), "AAAA".getBytes())
+//    p.addColumn("info".getBytes(), "age".getBytes(), "34".getBytes())
+//    p.addColumn("info".getBytes(), "gender".getBytes(), "男".getBytes())
+//    table.put(p)
 
-    val p = new Put(new String("4").getBytes)
-    p.addColumn("info".getBytes(), "name".getBytes(), "AAAA".getBytes())
-    p.addColumn("info".getBytes(), "age".getBytes(), "34".getBytes())
-    p.addColumn("info".getBytes(), "gender".getBytes(), "男".getBytes())
-    table.put(p)
+    val schemas = StructType(List(StructField("name", StringType, true),StructField("age", StringType, true),StructField("gender", StringType, true)))
+
+    val RDD=spark.sparkContext.newAPIHadoopRDD(conf,classOf[TableInputFormat],classOf[ImmutableBytesWritable],classOf[Result])
+    val v=RDD.values
+
+    val userDataFrame = spark.createDataFrame(RDD.values,classOf[Result])
+    userDataFrame.show()
 
     spark.stop()
   }
+
 
 }
--- src/main/scala/Tag.scala
+++ src/main/scala/Tag.scala
@@ -1,2 +1,6 @@
 case class Tag(UserId:String,MovieId:String,tag:String,timestamp:String) {
 }
+
+case class Student(name:String,age:String,gender:String){
+
+}
